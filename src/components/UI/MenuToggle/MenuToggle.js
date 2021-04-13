2b0f198033f8079ab64dbfc6132b9e20c23ec5d9
Dylan Hamada <dylan.hamada@gmail.com>
2021-04-12 21:52:08 -0400

    Fix Draggable Margin Issue
    - Fix Draggable margin issue that produced extra margins, by only
    setting bottom margin instead of top and bottom
    - Add 'isDragging' styles to Draggables
    - Fix onDragEnd error when dragging Draggable outside Droppable
    - Change position of NavMenu to fixed to avoid scrolling problem when
    main page content is taller than viewport
    - Change position of MenuToggle to fixed for the same reason, and adjust
    padding and background-color for better aesthetic when main content is
    scrolling

--- src/components/UI/MenuToggle/MenuToggle.js
+++ src/components/UI/MenuToggle/MenuToggle.js
@@ -4,7 +4,9 @@ import classes from './MenuToggle.module.css';
 
 const MenuToggle = props => {
     return (
-        <div className={classes.MenuToggle} onClick={props.menuToggle} title="Menu Toggle" />
+        <div className={classes.MenuToggle} title="Menu Toggle">
+            <div className={classes.ToggleButton} onClick={props.menuToggle}></div>
+        </div>
     );
 };
 
--- src/containers/Find/Criteria/Criteria.js
+++ src/containers/Find/Criteria/Criteria.js
@@ -9,49 +9,63 @@ class Criteria extends Component {
     }
 
     onDragEnd = (result) => {
-        let newCriteria = [...this.state.criteria];
-        newCriteria.splice(result.source.index, 1);
-        newCriteria.splice(result.destination.index, 0, result.draggableId);
-        this.setState({ criteria: newCriteria });
+        if (result.destination) {
+            let newCriteria = [...this.state.criteria];
+            newCriteria.splice(result.source.index, 1);
+            newCriteria.splice(result.destination.index, 0, result.draggableId);
+            this.setState({ criteria: newCriteria });
+        }
     }
 
     render() {
         return (
-            <DragDropContext
+            <React.Fragment>
+                <p className={styles.Text}>Drag and arrange the following vehicle qualities in order of importance, with the most important on top and least important at the bottom.</p>
+                <DragDropContext
                 onDragEnd={this.onDragEnd}
-            >
-                <Droppable 
-                    droppableId="criteria"
                 >
-                    {provided => (
-                        <div
-                            className={styles.Droppable}
-                            ref={provided.innerRef}
-                            {...provided.droppableProps}
-                        >
-                            {this.state.criteria.map((criteria, index) => (
-                                <Draggable
-                                    key={criteria}
-                                    draggableId={criteria}
-                                    index={index}    
-                                >
-                                    {(provided, snapshot) => (
-                                        <div
-                                            className={styles.Draggable}
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                        >
-                                            {criteria}
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
+                    <Droppable 
+                        droppableId="criteria"
+                    >
+                        {provided => (
+                            <div
+                                className={styles.Droppable}
+                                ref={provided.innerRef}
+                                {...provided.droppableProps}
+                            >
+                                {this.state.criteria.map((criteria, index) => (
+                                    <Draggable
+                                        key={criteria}
+                                        draggableId={criteria}
+                                        index={index}    
+                                    >
+                                        {(provided, snapshot) => {
+                                            const style = {
+                                                backgroundColor: snapshot.isDragging ? '#00A9EF' : '#DADADA',
+                                                color: snapshot.isDragging ? '#EEE' : '#444',
+                                                ...provided.draggableProps.style,
+                                            };
+
+                                            return (
+                                                <div
+                                                    className={styles.Draggable}
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                    style={style}
+                                                >
+                                                    {criteria}
+                                                </div>
+                                            );
+                                        }}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+            </React.Fragment>
         );
     }
 }
