6dad45651fa3fc037f7cfddfa06764c15d909a4d
Dylan Hamada <dylan.hamada@gmail.com>
2021-04-19 21:48:40 -0400

    Write selectOptionHandler Method
    - write selectOptionHandler method in Questionnaire container to update
    'selected' property in component state when user clicks on an option in
    the questionnaire
    - write conditoinal styles in Question ocmponent depending on whether
    the selected prop matches the option span being rendered
    - Add navigational links to Questionnaire container and style using
    flexbox

--- src/components/Find/Question/Question.js
+++ src/components/Find/Question/Question.js
@@ -4,8 +4,16 @@ import styles from './Question.module.css';
 
 const Question = props => {
     const questions = props.options.map((option, index) => {
+        const selected = index === props.selected ? styles.Selected : '';
+
         return (
-            <span key={'option-' + (index + 1)} className={styles.Option} onClick={props.click}>{option}</span>
+            <span 
+                key={'option-' + (index + 1)} 
+                className={`${styles.Option} ${selected}`} 
+                onClick={props.click}
+            >
+                    {option}
+            </span>
         );
     });
 
--- src/containers/Find/Questionnaire/Questionnaire.js
+++ src/containers/Find/Questionnaire/Questionnaire.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
+import styles from './Questionnaire.module.css';
 import Question from '../../../components/Find/Question/Question';
 
 import { scrollToTop } from '../../../shared/utility';
@@ -50,16 +52,41 @@ class Questionnaire extends Component {
     }
 
     selectOptionHandler = (event, questionIdentifier) => {
+        let updatedQuestion = {...this.state.questions[questionIdentifier]};
+        updatedQuestion.selected = null;
+        updatedQuestion.options.forEach((option, index) => {
+            if (option === event.target.innerHTML) {
+                updatedQuestion.selected = index;
+            }
+        });
+
+        let updatedQuestions = [...this.state.questions];
+        updatedQuestions[questionIdentifier] = updatedQuestion;
+        
+        this.setState({
+            questions: updatedQuestions
+        });
     }
 
     render () {
         const questions = this.state.questions.map((question, index) => (
-            <Question key={'question-' + (index + 1)} number={index + 1} question={question.question} options={question.options} click={(event) => this.selectOptionHandler(event, index)} />
+            <Question 
+                key={'question-' + (index + 1)} 
+                number={index + 1} 
+                question={question.question} 
+                options={question.options} 
+                selected={question.selected}
+                click={(event) => this.selectOptionHandler(event, index)} 
+            />
         ));
 
         return (
             <React.Fragment>
                 {questions}
+                <div className={styles.Links}>
+                    <Link to={'/find/criteria'} className={styles.Link}>Previous</Link>
+                    <Link to={'/find/criteria'} className={styles.Link}>Submit</Link>
+                </div>
             </React.Fragment>
         );
     }
