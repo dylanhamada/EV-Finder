70a8dcd61462f739b8341013af235606602549de
Dylan Hamada <dylan.hamada@gmail.com>
2021-05-26 20:17:30 -0400

    Convert Dashboard and NavMenu to Functional Comp
    - Convert Dashboard and NavMenu containers from class components to
    functional components

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,41 +1,47 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import styles from './Dashboard.module.css';
-import User from '../../components/User/User';
+import styles from "./Dashboard.module.css";
+import User from "../../components/User/User";
 
-class Dashboard extends Component {
-    state = {
-        userName: 'Dylan Hamada'
-    }
+const Dashboard = (props) => {
+  const [state, setState] = useState({
+    userName: "Dylan Hamada",
+  });
 
-    render() {
-        return (
-            <div className={styles.Dashboard}>
-                <User userName={this.state.userName} />
-                <ul className={styles.NavList}>
-                    <Link className={styles.Link} to="/find">
-                        <li className={styles.NavListItem}>
-                            <h3 className={styles.NavListItemLink}>FIND</h3>
-                            <p className={styles.NavListItemDescription}>Complete a questionnaire and get matched to an electric vehicle.</p>
-                        </li>
-                    </Link>
-                    <li className={styles.NavListItem}>
-                        <h3 className={styles.NavListItemLink}>Browse</h3>
-                        <p className={styles.NavListItemDescription}>Browse all of 2020's electric vehicles.</p>
-                    </li>
-                    <li className={styles.NavListItem}>
-                        <h3 className={styles.NavListItemLink}>Compare</h3>
-                        <p className={styles.NavListItemDescription}>Compare two electric vehicles head to head.</p>
-                    </li>
-                    <li className={styles.NavListItem}>
-                        <h3 className={styles.NavListItemLink}>Favorites</h3>
-                        <p className={styles.NavListItemDescription}>View a list of your favorites.</p>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
-}
+  return (
+    <div className={styles.Dashboard}>
+      <User userName={state.userName} />
+      <ul className={styles.NavList}>
+        <Link className={styles.Link} to="/find">
+          <li className={styles.NavListItem}>
+            <h3 className={styles.NavListItemLink}>FIND</h3>
+            <p className={styles.NavListItemDescription}>
+              Complete a questionnaire and get matched to an electric vehicle.
+            </p>
+          </li>
+        </Link>
+        <li className={styles.NavListItem}>
+          <h3 className={styles.NavListItemLink}>Browse</h3>
+          <p className={styles.NavListItemDescription}>
+            Browse all of 2020's electric vehicles.
+          </p>
+        </li>
+        <li className={styles.NavListItem}>
+          <h3 className={styles.NavListItemLink}>Compare</h3>
+          <p className={styles.NavListItemDescription}>
+            Compare two electric vehicles head to head.
+          </p>
+        </li>
+        <li className={styles.NavListItem}>
+          <h3 className={styles.NavListItemLink}>Favorites</h3>
+          <p className={styles.NavListItemDescription}>
+            View a list of your favorites.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Dashboard;
--- src/containers/NavMenu/NavMenu.js
+++ src/containers/NavMenu/NavMenu.js
@@ -1,55 +1,55 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import styles from './NavMenu.module.css';
+import styles from "./NavMenu.module.css";
 
-import CloseButton from '../../components/UI/CloseButton/CloseButton';
-import MenuToggle from '../../components/UI/MenuToggle/MenuToggle';
-import User from '../../components/User/User';
+import CloseButton from "../../components/UI/CloseButton/CloseButton";
+import MenuToggle from "../../components/UI/MenuToggle/MenuToggle";
+import User from "../../components/User/User";
 
-class NavMenu extends Component {
-    state = {
-        userName: 'Dylan Hamada',
-        menuOpen: false
-    }
+const NavMenu = (props) => {
+  const [state, setState] = useState({
+    userName: "Dylan Hamada",
+    menuOpen: false,
+  });
 
-    toggleHandler = () => {
-        this.setState((prevState) => ({
-            menuOpen: !prevState.menuOpen
-        }));
-    }
+  const toggleHandler = () => {
+    setState((prevState) => ({
+      menuOpen: !prevState.menuOpen,
+    }));
+  };
 
-    linkClickHandler = () => {
-        this.setState({
-            menuOpen: false
-        });
-    }
+  const linkClickHandler = () => {
+    setState({
+      menuOpen: false,
+    });
+  };
 
-    render() {
-        let menuClasses = [styles.NavMenu, styles.Close];
-        if (this.state.menuOpen) {
-            menuClasses = [styles.NavMenu, styles.Open];
-        }
+  let menuClasses = [styles.NavMenu, styles.Close];
+  if (state.menuOpen) {
+    menuClasses = [styles.NavMenu, styles.Open];
+  }
 
-        return (
-            <React.Fragment>
-                <MenuToggle menuToggle={this.toggleHandler} />
-                <nav className={menuClasses.join(' ')}>
-                    <CloseButton menuToggle={this.toggleHandler} />
-                    <User userName={this.state.userName} />
-                    <ul className={styles.NavList}>
-                        <Link className={styles.Link} to="/find">
-                            <li className={styles.NavListItem} onClick={this.linkClickHandler}>Find</li>
-                        </Link>
-                        <li className={styles.NavListItem}>Browse</li>
-                        <li className={styles.NavListItem}>Compare</li>
-                        <li className={styles.NavListItem}>Favorites</li>
-                        <li className={styles.NavListItem}>Log Out</li>
-                    </ul>
-                </nav>
-            </React.Fragment>
-        );
-    }
-}
+  return (
+    <React.Fragment>
+      <MenuToggle menuToggle={toggleHandler} />
+      <nav className={menuClasses.join(" ")}>
+        <CloseButton menuToggle={toggleHandler} />
+        <User userName={state.userName} />
+        <ul className={styles.NavList}>
+          <Link className={styles.Link} to="/find">
+            <li className={styles.NavListItem} onClick={linkClickHandler}>
+              Find
+            </li>
+          </Link>
+          <li className={styles.NavListItem}>Browse</li>
+          <li className={styles.NavListItem}>Compare</li>
+          <li className={styles.NavListItem}>Favorites</li>
+          <li className={styles.NavListItem}>Log Out</li>
+        </ul>
+      </nav>
+    </React.Fragment>
+  );
+};
 
 export default NavMenu;
