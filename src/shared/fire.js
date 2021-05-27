97fc78d41d429c7a796302f8880be507c6d11d09
Dylan Hamada <dylan.hamada@gmail.com>
2021-05-26 21:58:35 -0400

    Create Firebase Export File
    - Create Firebase export file so Firebase functionality can be used in
    multiple components

---
+++ src/shared/fire.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhVAKeeNSYXuQfgMbuaY7g7aSqEFTYxcw",
+  authDomain: "ev-finder-3b5dd.firebaseapp.com",
+  databaseURL: "https://ev-finder-3b5dd-default-rtdb.firebaseio.com",
+  projectId: "ev-finder-3b5dd",
+  storageBucket: "ev-finder-3b5dd.appspot.com",
+  messagingSenderId: "1004964500597",
+  appId: "1:1004964500597:web:2369be412fd30d322f943c"
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
