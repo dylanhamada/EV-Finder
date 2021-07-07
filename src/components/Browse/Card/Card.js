f4a841957a1587dbcb93ae9b4ad531ec8e99b8f6
Dylan Hamada <dylan.hamada@gmail.com>
2021-07-06 20:19:27 -0400

    Create Horizontally-Scrolling Showcase Container
    - Modify Scrollable container to scroll any x-overflow
    - Create a ScrollableContainer child within Scrollable to contain Card
    components
    - Adjust ScrollableContainer CSS to make it scroll horizontally and
    include a column gap
    - Adjust Card CSS to add a left and right margin to first and last
    instances

--- src/components/Browse/Card/Card.js
+++ src/components/Browse/Card/Card.js
@@ -3,7 +3,20 @@ import React from "react";
 import styles from "./Card.module.css";
 
 const Card = (props) => {
-  return <div className={styles.Card}>Card</div>;
+  let card = <div className={styles.Card}>Card</div>;
+
+  switch (props.end) {
+    case "first":
+      card = <div className={`${styles.Card} ${styles.CardFirst}`}>Card</div>;
+      break;
+    case "last":
+      card = <div className={`${styles.Card} ${styles.CardLast}`}>Card</div>;
+      break;
+    default:
+      card = <div className={styles.Card}>Card</div>;
+  }
+
+  return card;
 };
 
 export default Card;
--- src/components/Browse/Scrollable/Scrollable.js
+++ src/components/Browse/Scrollable/Scrollable.js
@@ -4,28 +4,16 @@ import styles from "./Scrollable.module.css";
 import Card from "../Card/Card";
 
 const Scrollable = (props) => {
-  const divStyle = {
-    columnGap: "10px",
-    display: "grid",
-    gridAutoFlow: "column",
-  };
-
-  const spacerStyle = {
-    width: "30px",
-  };
-
   return (
     <div className={styles.Scrollable}>
-      <div style={spacerStyle} />
-      <div style={divStyle}>
+      <div className={styles.ScrollableContainer}>
+        <Card end="first" />
         <Card />
         <Card />
         <Card />
         <Card />
-        <Card />
-        <Card />
+        <Card end="last" />
       </div>
-      <div style={spacerStyle} />
     </div>
   );
 };
