429e815b7d8a2a3ae476f8ce8e1d48639a94aa35
Dylan Hamada <dylan.hamada@gmail.com>
2021-10-06 21:40:32 -0400

    Clicking Selector Pushes to Browse
    - useHistory.push() in Selector component directs to Browse container
    when Selector is clicked

--- src/components/Compare/Selector/Selector.js
+++ src/components/Compare/Selector/Selector.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import styles from "./Selector.module.css";
 
 const Selector = (props) => {
+  const history = useHistory();
   let which = props.which === "one" ? "One" : "Two";
 
+  const compareLinkHandler = () => history.push(`/browse`);
+
   return (
-    <div className={styles.Selector}>
+    <div className={styles.Selector} onClick={() => compareLinkHandler()}>
       <h3 className={styles.Title}>Vehicle {which}</h3>
     </div>
   );
