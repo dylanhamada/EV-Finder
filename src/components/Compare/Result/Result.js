c09379534f9b097dd49bdf77a0529ef7452b715f
Dylan Hamada <dylan.hamada@gmail.com>
2021-11-03 21:48:52 -0400

    Create specComparison Array in Result Component
    - In Result component, create specComparison array that includes whether
    current vehicle specs are superior or inferior to other vehicle
    - Render Specs component inside Result component
    - Add number values for specs in vehicleInfo
    - Add Inferior and Superior styles in Specs component to conditionally
    change spec colors

--- src/components/Compare/Result/Result.js
+++ src/components/Compare/Result/Result.js
@@ -1,26 +1,53 @@
 import React from "react";
 
 import Showcase from "../../../containers/Showcase/Showcase";
+import Specs from "../../Info/Specs/Specs";
 
 const Result = (props) => {
   const order = [
-    "priceNum",
-    "rangeNum",
+    ["priceNum", "less"],
+    ["rangeNum", "greater"],
     null,
     null,
-    "horsepowerNum",
-    "chargingTimeNum",
-    "battery",
-    "fuel economy",
-    "brakes",
-    "cargo capacity",
-    "dimensions",
-    "weight",
+    ["horsepowerNum", "greater"],
+    ["chargingTimeNum", "less"],
+    ["batteryNum", "greater"],
+    ["fuelEconomyNum", "greater"],
+    null,
+    ["cargoNum", "greater"],
+    null,
+    null,
   ];
+  const currentNum = props.num;
+  const otherNum = props.num === 1 ? 2 : 1;
+  const currentVehicle = props.compareState[currentNum];
+  const otherVehicle = props.compareState[otherNum];
+
+  const specComparison = order.map((spec) => {
+    let specValue = null;
+
+    if (spec) {
+      const specName = spec[0];
+      const specConditional = spec[1];
+
+      if (specConditional === "less") {
+        specValue = currentVehicle[specName] < otherVehicle[specName];
+      }
+      if (specConditional === "greater") {
+        specValue = currentVehicle[specName] > otherVehicle[specName];
+      }
+    }
+
+    return specValue;
+  });
 
   return (
     <React.Fragment>
       <Showcase vehicle={props.compareState[props.num]} />
+      <Specs
+        specs={props.compareState[props.compareState[0]]}
+        comparison={specComparison}
+      />
     </React.Fragment>
   );
 };
