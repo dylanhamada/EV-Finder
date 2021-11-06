3334f5063ddf149b40c6eaef10ff465b69dd875d
Dylan Hamada <dylan.hamada@gmail.com>
2021-11-05 23:08:48 -0400

    figureStyle Variable in Specs Component
    - Create figureStyle var in Specs component to conditionally set color
    of vehicle spec

--- src/components/Info/Specs/Specs.js
+++ src/components/Info/Specs/Specs.js
@@ -17,16 +17,32 @@ const Specs = (props) => {
     "dimensions",
     "weight",
   ];
+  let figureStyle = null;
 
   if (props.snapshot) {
     order = props.snapshot;
   }
 
-  const details = order.map((detail) => {
+  const details = order.map((detail, index) => {
+    if (props.comparison) {
+      switch (props.comparison[index]) {
+        case true:
+          figureStyle = styles.Superior;
+          break;
+        case false:
+          figureStyle = styles.Inferior;
+          break;
+        default:
+          figureStyle = null;
+      }
+    }
+
     return (
       <div key={detail}>
         <p className={styles.Detail}>{detail}</p>
-        <p className={styles.Figure}>{props.specs[detail]}</p>
+        <p className={`${styles.Figure} ${figureStyle}`}>
+          {props.specs[detail]}
+        </p>
       </div>
     );
   });
